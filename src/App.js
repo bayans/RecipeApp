a1c42bba8dbdbff5d7ad368d8c579c8a3333a7a3
baian <bay9808@gmail.com>
2023-11-17 21:01:22 +0100

    Companionship Question Page Added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MoodQuestion from './pages/MoodQuestion';
+import CompanionshipQuestion from './pages/CompanionshipQuestion';
 import Login from './pages/Login';
 
 const App = () => {
 
   const [mood, setMood] = useState(null);
+  const [companionship, setCompanionship] = useState(null);
 
   return (
     <Routes>
       <Route path="/mood-question" element={<MoodQuestion moodHandler={setMood} />} />
+      <Route path="/companionship-question" element={<CompanionshipQuestion mood={mood} companionshipHandler={setCompanionship} />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
